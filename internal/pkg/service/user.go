b09b7810de153f94e492ff07b31ee19820cd7d34
Hanif Naufal <kakaknaufal@gmail.com>
2024-05-28 22:47:34 +0700

    fix:
    - fix register logic

--- internal/pkg/service/user.go
+++ internal/pkg/service/user.go
@@ -7,47 +7,42 @@ import (
 	"beli-mang/internal/pkg/middleware"
 	"net/http"
 
-	"github.com/jackc/pgx/v5/pgconn"
 	"golang.org/x/crypto/bcrypt"
 )
 
-func (s *Service) RegisterUser(body model.User) errs.Response {
+func (s *Service) RegisterUser(body model.User) (*dto.AuthResponse, errs.Response) {
 	var err error
-
 	db := s.DB()
 
+	// check if username and email with same role is already exist
+	var usernameCount, emailCount int
+	stmt := `SELECT 
+    (SELECT COUNT(*) FROM users WHERE username = $1) AS username_count,
+    (SELECT COUNT(*) FROM users WHERE email = $2 AND role = $3) AS email_count`
+	if err := db.QueryRow(stmt, body.Username, body.Email, body.Role).Scan(&usernameCount, &emailCount); err != nil {
+		return nil, errs.NewInternalError("check user error", err)
+	}
+
+	if usernameCount > 0 {
+		return nil, errs.NewGenericError(http.StatusConflict, "username is already exist")
+	}
+
+	if emailCount > 0 {
+		return nil, errs.NewGenericError(http.StatusConflict, "email is already exist in the same role")
+	}
+
 	// insert user by role
-	stmt := "INSERT INTO users (username, email, password_hash, role, email_role) VALUES ($1, $2, $3, $4, $5)"
-	if _, err = db.Exec(stmt, body.Username, body.Email, body.PasswordHash, body.Role, body.EmailRole); err != nil {
-		return errs.NewInternalError("insert error", err)
+	stmt = "INSERT INTO users (username, email, password_hash, role) VALUES ($1, $2, $3, $4)"
+	if _, err := db.Exec(stmt, body.Username, body.Email, body.PasswordHash, body.Role); err != nil {
+		return nil, errs.NewInternalError("insert error", err)
 	}
 
+	token, err := middleware.JWTSign(s.Config(), body.Username, body.Role)
 	if err != nil {
-		if pgErr, ok := err.(*pgconn.PgError); ok {
-			if pgErr.Code == "23505" {
-				errs.NewGenericError(http.StatusConflict, "user/admin is conflict")
-			}
-		}
-		return errs.NewInternalError("insert error", err)
+		return nil, errs.NewInternalError("token signing error", err)
 	}
 
-	// generate token
-	var token string
-	if body.Role == "it" {
-		token, err = middleware.JWTSign(s.Config(), body.Username, body.Role)
-		if err != nil {
-			return errs.NewInternalError("token signing error", err)
-		}
-	}
-
-	// TODO: Implement this method
-	return errs.Response{
-		Code:    http.StatusOK,
-		Message: "User registered successfully",
-		Data: dto.AuthResponse{
-			Token: token,
-		},
-	}
+	return &dto.AuthResponse{Token: token}, errs.Response{}
 }
 
 func (s *Service) LoginUser(body model.User) errs.Response {
@@ -59,13 +54,12 @@ func (s *Service) LoginUser(body model.User) errs.Response {
 	db := s.DB()
 
 	// check NIP in database
-	stmt := "SELECT username, email, password_hash, role, email_role FROM users WHERE username = $1"
+	stmt := "SELECT username, email, password_hash, role FROM users WHERE username = $1"
 	if err := db.QueryRow(stmt, body.Username).Scan(
 		&out.Username,
 		&out.Email,
 		&out.PasswordHash,
 		&out.Role,
-		&out.EmailRole,
 	); err != nil {
 		return errs.NewNotFoundError(errs.ErrUserNotFound)
 	}
