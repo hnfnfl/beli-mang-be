46cf90d1c6ca11039794e02ad77f4d65690a7d7f
rois.alhaqq <rois.alhaqq@Muhammad-Rois-Alhaqq.local>
2024-05-28 21:14:29 +0700

    update merge message

--- internal/db/migrations/20240508102205_init_table_users.down.sql
+++ internal/db/migrations/20240508102205_init_table_users.down.sql
@@ -1,8 +1,2 @@
-<<<<<<< Updated upstream
-DROP TABLE IF EXISTS staff CASCADE;
-
-DROP TABLE IF EXISTS country_code CASCADE;
-=======
 DROP TABLE IF EXISTS users;
 DROP TYPE IF EXISTS role_user;
->>>>>>> Stashed changes
--- internal/db/migrations/20240508102205_init_table_users.up.sql
+++ internal/db/migrations/20240508102205_init_table_users.up.sql
@@ -2,19 +2,6 @@ CREATE TYPE IF NOT EXISTS role_user AS ENUM ('admin', 'user');
 
 CREATE TABLE IF NOT EXISTS users (
     username VARCHAR(30) PRIMARY KEY NOT NULL,
-<<<<<<< Updated upstream
-    email VARCHAR(30) UNIQUE NOT NULL,
-    password_hash VARCHAR(255) NOT NULL,
-    role role_user,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE INDEX idx_username ON users(username);
-
-CREATE INDEX idx_email ON users(email);
-
-CREATE INDEX idx_role ON users(role);
-=======
     email VARCHAR(30) NOT NULL,
     password_hash VARCHAR(255) NOT null,
     role role_user,
@@ -25,4 +12,3 @@ CREATE INDEX idx_role ON users(role);
 CREATE INDEX idx_username on users(username);
 CREATE INDEX idx_email_role on users(email_role);
 CREATE INDEX idx_role on users(role);
->>>>>>> Stashed changes
