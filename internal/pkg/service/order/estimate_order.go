d167aa2e8dbed0a53a754919fab02f7868a086d2
Hanif Naufal <kakaknaufal@gmail.com>
2024-05-30 18:12:36 +0700

    fix:
    - add username to gin from middleware
    - fix estimate logic
    - add cache util

--- internal/pkg/service/order/estimate_order.go
+++ internal/pkg/service/order/estimate_order.go
@@ -4,46 +4,34 @@ import (
 	"beli-mang/internal/pkg/dto"
 	"beli-mang/internal/pkg/errs"
 	"beli-mang/internal/pkg/util"
-	"encoding/json"
 	"fmt"
 	"strings"
-	"sync"
 	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
+var cache = util.NewCache()
+
 func (s *OrderService) EstimateOrder(ctx *gin.Context, data dto.OrderEstimateRequest) *dto.OrderEstimateResponse {
 	db := s.db
 	var (
 		startingMerchant dto.Orders
-		checkItem        string
-		checkMerchant    string
 		stmt             strings.Builder
 		calculateItems   []dto.OrdersItems
+
+		checkMerchantIds = make([]string, 0)
+		checkItemIds     = make([]string, 0)
 	)
 
-	var (
-		cache      = make(map[string]dto.CacheItem)
-		cacheMutex = &sync.Mutex{}
-		cacheTTL   = 24 * time.Hour // Time-to-live for cache items
+	username := ctx.Value("username").(string)
+	timeStamp := time.Now().UnixNano()
+	calculatedEstimateId := fmt.Sprintf("%s-%d", username, timeStamp)
 
-	)
-
-	dataJSON, err := json.Marshal(data)
-	if err != nil {
-		return nil
+	// check if the request is already cached
+	if cachedResponse, exist := cache.Get(calculatedEstimateId); exist {
+		return cachedResponse.(*dto.OrderEstimateResponse)
 	}
-
-	calculatedEstimateId := string(dataJSON)
-
-	cacheMutex.Lock()
-	cachedItem, exists := cache[calculatedEstimateId]
-	if exists && time.Since(cachedItem.CachedAt) < cacheTTL {
-		cacheMutex.Unlock()
-		return &cachedItem.Response
-	}
-	cacheMutex.Unlock()
 
 	totalPrice := 0.0
 	userLat, userLong := data.UserLocation.Lat, data.UserLocation.Long
@@ -55,13 +43,10 @@ func (s *OrderService) EstimateOrder(ctx *gin.Context, data dto.OrderEstimateReq
 		}
 	}
 
-	checkItem = "("
-	checkMerchant = "("
-
 	for _, order := range data.Orders {
-		checkMerchant = fmt.Sprintf("'%s', ", order.MerchantId)
+		checkMerchantIds = append(checkMerchantIds, fmt.Sprintf("'%s'", order.MerchantId))
 		for _, item := range order.Items {
-			checkItem = fmt.Sprintf("'%s', ", item.ItemId)
+			checkItemIds = append(checkItemIds, fmt.Sprintf("'%s'", item.ItemId))
 			calculateItems = append(calculateItems, dto.OrdersItems{
 				ItemId:   item.ItemId,
 				Quantity: item.Quantity,
@@ -69,30 +54,24 @@ func (s *OrderService) EstimateOrder(ctx *gin.Context, data dto.OrderEstimateReq
 		}
 	}
 
-	checkItem = string(checkItem)[:len(checkItem)-1]
-	checkMerchant = string(checkMerchant[:len(checkMerchant)-1])
-
-	checkItem = fmt.Sprintf("%s)", checkItem)
-	checkMerchant = fmt.Sprintf("%s)", checkMerchant)
+	checkMerchant := fmt.Sprintf("(%s)", strings.Join(checkMerchantIds, ","))
+	checkItem := fmt.Sprintf("(%s)", strings.Join(checkItemIds, ","))
 
 	stmt.WriteString(fmt.Sprintf("SELECT item_id, price FROM merchant_items WHERE merchant_id IN %s and item_id IN %s", checkMerchant, checkItem))
 
 	rows, err := db.Query(ctx, stmt.String())
 	if err != nil {
+		errs.NewInternalError(ctx, "Failed to query merchant items", err)
 		return nil
 	}
 	defer rows.Close()
 
-	var items []dto.OrderEstimateItemPrice
 	for rows.Next() {
 		var item dto.OrderEstimateItemPrice
 		if err := rows.Scan(&item.ItemId, &item.Price); err != nil {
 			return nil
 		}
-		items = append(items, item)
-	}
 
-	for _, item := range items {
 		for _, calculateItem := range calculateItems {
 			if calculateItem.ItemId == item.ItemId {
 				totalPrice += float64(item.Price) * float64(calculateItem.Quantity)
@@ -101,10 +80,11 @@ func (s *OrderService) EstimateOrder(ctx *gin.Context, data dto.OrderEstimateReq
 	}
 
 	stmt.Reset()
-	stmt.WriteString(fmt.Sprintf("SELECT lat, long FROM (SELECT lat, long, CASE WHEN merchant_id = %s THEN 1 ELSE 0 END AS start_merchant FROM merchants WHERE merchant_id IN %s) ORDER BY start_merchant DESC", startingMerchant.MerchantId, checkMerchant))
+	stmt.WriteString(fmt.Sprintf("SELECT lat, long FROM (SELECT lat, long, CASE WHEN merchant_id = '%s' THEN 1 ELSE 0 END AS start_merchant FROM merchants WHERE merchant_id IN %s) AS subquery ORDER BY start_merchant DESC", startingMerchant.MerchantId, checkMerchant))
 
 	rows, err = db.Query(ctx, stmt.String())
 	if err != nil {
+		errs.NewInternalError(ctx, "Failed to query merchants", err)
 		return nil
 	}
 	defer rows.Close()
@@ -134,13 +114,8 @@ func (s *OrderService) EstimateOrder(ctx *gin.Context, data dto.OrderEstimateReq
 		CalculatedEstimateId:           calculatedEstimateId,
 	}
 
-	cacheMutex.Lock()
-	cache[calculatedEstimateId] = dto.CacheItem{
-		Request:  data,
-		Response: response,
-		CachedAt: time.Now(),
-	}
-	cacheMutex.Unlock()
+	// cache the response if it's not already cached
+	cache.Set(calculatedEstimateId, &response, 24*time.Hour)
 
 	return &response
 }
